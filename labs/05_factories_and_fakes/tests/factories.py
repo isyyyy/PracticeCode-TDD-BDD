6700b6c2787b9c875ba54cb03956eb6ab50ea430
tuanmmp <tuan.pham@missmp.eu>
2024-07-15 23:47:31 +0700

    feat: update test factories and faker

--- labs/05_factories_and_fakes/tests/factories.py
+++ labs/05_factories_and_fakes/tests/factories.py
@@ -9,9 +9,10 @@ Documentation on Fuzzy Attributes:
 
 """
 import factory
-from datetime import date
+from datetime import date, datetime
 from factory.fuzzy import FuzzyChoice, FuzzyDate
 from models.account import Account
+
 
 class AccountFactory(factory.Factory):
     """ Creates fake Accounts """
@@ -20,3 +21,10 @@ class AccountFactory(factory.Factory):
         model = Account
 
     # Add attributes here...
+    id = factory.Sequence(lambda n: n)
+    name = factory.Faker("name")
+    email = factory.Faker("email")
+    phone_number = factory.Faker("phone_number")
+    disabled = FuzzyChoice([True, False])
+    # date_joined = factory.LazyFunction(datetime.utcnow)
+    date_joined = FuzzyDate(date(2020, 1, 1), date(2021, 1, 1))
